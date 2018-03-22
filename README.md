24c2333fcabc8caf04a48884ab9e341551cba37f
Unknown <chadwick.gay@connect.qut.edu.au>
2018-03-22 16:35:06 +1000

    Repo migration

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-GovHack-2017
+# GovHack 2017
+
+## Team Name: Chadjango
+
+## Members: Chad Gay, Brody Ly, Michael Esteban
+
+### We have built a web app designed to assist patrons who will be attending the Commonwealth Games.
+
+### The app aims to display the main sites of the game, and free pubic amenities that are nearby such as water fountains, bicycle racks and showers. When an amenity is clicked on, the street view is shown to give the user a better indication of its location
+
+### The tool currently supports Gold Coast data on public drinking fountains, bicycle racks and showers. Further data sets could be easily added. 
+
+
+##### Project demo - http://gold-coast.surge.sh/
---
+++ venues.js
@@ -0,0 +1,80 @@
+var venues = [
+    {
+        "name": "Carrara Stadium",
+        "events": "Athletics, Opening Ceremony, Closing Ceremony[1]",
+        "lat": -28.00383332,
+        "lng": 153.3669985
+  },
+    {
+        "name": "Robina Stadium",
+        "events": "Rugby 7s",
+        "lat": -28.066797,
+        "lng": 153.378796
+  },
+    {
+        "name": "Gold Coast Aquatic Centre",
+        "events": "Swimming, Diving",
+        "lat": -27.963879,
+        "lng": 153.416022
+  },
+    {
+        "name": "Coomera Sport and Leisure Centre",
+        "events": "Gymnastics, Netball (finals)",
+        "lat": -27.866472,
+        "lng": 153.319224
+  },
+    {
+        "name": "Gold Coast Convention and Exhibition Centre",
+        "events": "Basketball (finals), Netball(preliminaries)",
+        "lat": -28.027945,
+        "lng": 153.428611
+  },
+    {
+        "name": "Gold Coast Hockey Centre",
+        "events": "Hockey",
+        "lat": -27.954397,
+        "lng": 153.392572
+  },
+    {
+        "name": "Gold Coast Sports and Leisure Centre",
+        "events": "Badminton, Powerlifting, Weightlifting, Wrestling",
+        "lat": -28.009867,
+        "lng": 153.366716
+  },
+    {
+        "name": "Village Roadshow Studios",
+        "events": "Boxing, Table Tennis",
+        "lat": -27.913266,
+        "lng": 153.313335
+  },
+    {
+        "name": "Runaway Bay Sports Centre",
+        "events": "Squash",
+        "lat": -27.908909,
+        "lng": 153.39654
+  },
+    {
+        "name": "Broadbeach Bowls Club",
+        "events": "Lawn bowling",
+        "lat": -28.021171,
+        "lng": 153.431654
+  },
+    {
+        "name": "Carrara Indoor Stadium",
+        "events": "Weightlifting",
+        "lat": -28.005616,
+        "lng": 153.365324
+  },
+    {
+        "name": "Southport Broadwater Parklands",
+        "events": "Marathon, Racewalking, Triathlon",
+        "lat": -27.965906,
+        "lng": 153.417206
+  },
+    {
+        "name": "Queen Elizabeth Park[2]",
+        "events": "Beach Volleyball",
+        "lat": -28.167395,
+        "lng": 153.540234
+  }
+]
